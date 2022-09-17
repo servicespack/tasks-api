9ed981845890eed0877a46f6365b75a2b334783c
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-17 12:00:04 -0300

    Pretty log

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import pino from 'pino-http'
 
+import { LOGGER_OPTIONS } from './logger'
+
 const app = express()
 
-app.use(pino())
+app.use(pino(LOGGER_OPTIONS))
 
 export { app }
--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,21 @@
 import pino from 'pino'
 
-const logger = pino()
+const {
+  NODE_ENV = 'development'
+} = process.env
 
-export { logger }
+const LOGGER_OPTIONS = {
+  ...(
+    NODE_ENV === 'development'
+      ? {
+          transport: {
+            target: 'pino-pretty'
+          }
+        }
+      : {}
+  )
+}
+
+const logger = pino(LOGGER_OPTIONS)
+
+export { logger, LOGGER_OPTIONS }
