44643c73326c3ced678d5906783d38c5a3b110bd
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-17 11:36:01 -0300

    Initial logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import pino from 'pino-http'
 
 const app = express()
+
+app.use(pino())
 
 export { app }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { app } from './app'
+import { logger } from './logger'
 
 async function main (): Promise<void> {
   const {
     PORT = 3000
   } = process.env
 
-  app.listen(PORT)
+  app.listen(PORT, () => logger.info(`Listening on ${PORT}`))
 }
 
 main()
-  .catch(console.error)
+  .catch(logger.error)
