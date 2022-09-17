bb0ce6072703c382628fa09ecf80dbb8f36bd6c3
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-17 14:36:59 -0300

    Using helmet

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import helmet from 'helmet'
 import pino from 'pino-http'
 
 import { LOGGER_OPTIONS } from './logger'
 
 const app = express()
 
+app.use(helmet())
 app.use(pino(LOGGER_OPTIONS))
 
 export { app }
