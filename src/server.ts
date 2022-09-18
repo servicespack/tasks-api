81e2f0e95a58c6abf8c153c829e2cceba61bb095
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-17 23:39:19 -0300

    Update healthcheck

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import helmet from 'helmet'
 import http from 'http'
 import pino from 'pino-http'
 
-import { logger, LOGGER_OPTIONS } from './logger'
+import { LOGGER_OPTIONS } from './logger'
 import { router } from './routers/tasks.router'
 import { database } from './database'
 
@@ -14,20 +14,11 @@ app.use(express.json())
 app.use(helmet())
 app.use(pino(LOGGER_OPTIONS))
 
-app.get('/health', (_request, response) => {
-  Promise.all([
-    database.isConnected()
-  ])
-    .then(([database]) => {
-      response.json({
-        server: server.listening,
-        database
-      })
-    })
-    .catch((...errors) => {
-      logger.error(errors)
-      response.status(500)
-    })
+app.get('/health', async (request, response) => {
+  return response.json({
+    server: server.listening,
+    database: await database.isConnected()
+  })
 })
 
 app.use('/tasks', router)
