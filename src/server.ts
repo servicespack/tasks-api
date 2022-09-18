c3235973b3f3a50b883f69ffa05b9dc6a785f358
Gabriel Rufino <contato@gabrielrufino.com>
2022-09-18 13:05:17 -0300

    Auth

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt'
 import express from 'express'
 import helmet from 'helmet'
 import http from 'http'
+import passport from 'passport'
 import pino from 'pino-http'
 
 import { LOGGER_OPTIONS } from './logger'
 import { database } from './database'
 import { router } from './routers/tasks.router'
+
+const { JWT_SECRET } = process.env
+
+passport.use(
+  new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: JWT_SECRET
+  }, (payload, done) => {
+    const owner = { id: payload.sub }
+    done(null, owner)
+  })
+)
 
 const app = express()
 const server = http.createServer(app)
@@ -14,13 +28,13 @@ app.use(express.json())
 app.use(helmet())
 app.use(pino(LOGGER_OPTIONS))
 
-app.get('/health', async (request, response) => {
+app.get('/health', async (_request, response) => {
   return response.json({
     server: server.listening,
     database: await database.isConnected()
   })
 })
 
-app.use('/tasks', router)
+app.use('/tasks', passport.authenticate('jwt', { session: false }), router)
 
 export { server }
