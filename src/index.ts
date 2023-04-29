4ce0e19e84b7072cd41dadf6e8dc8ff2ec3a927a
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-29 14:27:52 -0300

    PORT => HTTP_SERVER_PORT

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { server } from './http.server'
 
 async function main (): Promise<void> {
   const {
-    PORT = 3000
+    HTTP_SERVER_PORT = 3000
   } = process.env
 
   await database.init()
 
-  server.listen(PORT, () => { logger.info(`Server listening on ${PORT}`) })
+  server.listen(HTTP_SERVER_PORT, () => { logger.info(`Server listening on ${HTTP_SERVER_PORT}`) })
 
   process.on('SIGTERM', async () => {
     server.close()
