514be10a4f5168255dd856e3f1c8739e7651a87a
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-29 17:46:42 -0300

    New environment variable: DATABASE_DRIVER

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,17 @@
-import { type Connection, type EntityManager, type IDatabaseDriver, MikroORM, type Options } from '@mikro-orm/core'
+import { type Connection, type EntityManager, type IDatabaseDriver, MikroORM, type Options, Configuration } from '@mikro-orm/core'
 
 import { NodeEnv } from './enumerables/node-env.enum'
 import { Task } from './entities/task.entity'
 
 const {
+  DATABASE_DRIVER = 'sqlite',
   DATABASE_URI = 'data.db',
   NODE_ENV = NodeEnv.DEVELOPMENT
 } = process.env
 
 export class Database {
   private readonly config: Options = {
-    type: 'sqlite',
+    type: DATABASE_DRIVER as any,
     dbName: DATABASE_URI,
     entities: [Task],
     debug: NODE_ENV === NodeEnv.DEVELOPMENT
