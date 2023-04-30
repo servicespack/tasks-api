2553e7017e504a800214c7e35e5eb1518021d596
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-30 15:42:46 -0300

    Update index.js - Using top level await

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,22 @@ import { database } from './database'
 import { logger } from './logger'
 import { server } from './http.server'
 
-async function main (): Promise<void> {
-  const {
-    HTTP_SERVER_ENABLED = 'true',
-    HTTP_SERVER_PORT = 3000
-  } = process.env
+const {
+  HTTP_SERVER_ENABLED = 'true',
+  HTTP_SERVER_PORT = 3000
+} = process.env
 
-  await database.init()
+await database.init()
 
-  if (HTTP_SERVER_ENABLED === 'true') {
-    server.listen(HTTP_SERVER_PORT, () => { logger.info(`Server listening on ${HTTP_SERVER_PORT}`) })
-  }
-
-  process.on('SIGTERM', async () => {
-    server.close()
-    await database.close()
-  })
+if (HTTP_SERVER_ENABLED === 'true') {
+  server.listen(HTTP_SERVER_PORT, () => { logger.info(`Server listening on ${HTTP_SERVER_PORT}`) })
 }
 
-main()
-  .catch(logger.error)
+process.on('SIGTERM', async () => {
+  server.close()
+  await database.close()
+})
+
+process
+  .on('unhandledRejection', logger.error)
+  .on('uncaughtException', logger.error)
