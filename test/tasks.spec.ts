4ff6e3dc9002f6a5ae767bc441de56157309f821
Gabriel Rufino <contato@gabrielrufino.com>
2023-07-07 20:32:21 -0300

    Fixing tests

--- test/tasks.spec.ts
+++ test/tasks.spec.ts
@@ -17,7 +17,7 @@ describe('Tasks', () => {
   beforeAll(async () => {
     process.env = {
       ...process.env,
-      DATABASE_URI: path.join(os.tmpdir(), '@gabrielrufino/tasks-api', faker.datatype.string()),
+      DATABASE_URI: path.join(os.tmpdir(), '@servicespack/tasks-api', faker.datatype.string()),
       JWT_SECRET: faker.lorem.word()
     }
 
