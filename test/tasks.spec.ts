9b07d5c2f3cac50503bdefafd7b1971ee7b9763c
Gabriel Rufino <contato@gabrielrufino.com>
2023-07-07 20:44:42 -0300

    ESLint for tests

--- test/tasks.spec.ts
+++ test/tasks.spec.ts
@@ -1,61 +1,61 @@
-import { afterAll, afterEach, beforeAll, describe, expect, it } from '@jest/globals'
-import { Server } from 'http'
-import { faker } from '@faker-js/faker'
-import njwt from 'njwt'
-import os from 'os'
-import path from 'path'
-import request from 'supertest'
+import { Server } from 'http';
+import os from 'os';
+import path from 'path';
 
-import { Database } from '../src/database'
-import { Task } from '../src/entities/task.entity'
-import { TaskStatus } from '../src/enumerables/task-status.enum'
+import { faker } from '@faker-js/faker';
+import njwt from 'njwt';
+import request from 'supertest';
+
+import { Database } from '../src/database';
+import { Task } from '../src/entities/task.entity';
+import { TaskStatus } from '../src/enums/task-status.enum';
 
 describe('Tasks', () => {
-  let database: Database
-  let server: Server
+  let database: Database;
+  let server: Server;
 
   beforeAll(async () => {
     process.env = {
       ...process.env,
       DATABASE_URI: path.join(os.tmpdir(), '@servicespack/tasks-api', faker.datatype.string()),
-      JWT_SECRET: faker.lorem.word()
-    }
+      JWT_SECRET: faker.lorem.word(),
+    };
 
     database = (
       await import(path.join(__dirname, '..', 'src', 'database'))
-    ).database
-    await database.init()
+    ).database;
+    await database.init();
 
     server = (
       await import(path.join(__dirname, '..', 'src', 'http.server'))
-    ).server
-  })
+    ).server;
+  });
 
   afterEach(async () => {
-    await database.em.nativeDelete(Task, {})
-  })
+    await database.em.nativeDelete(Task, {});
+  });
 
   afterAll(async () => {
-    await database.close()
-  })
+    await database.close();
+  });
 
   describe('POST /tasks', () => {
     it('Should create a new task correctly in the ideal case', async () => {
-      const sub = faker.datatype.uuid()
-      const token = `Bearer ${njwt.create({ sub }, process.env.JWT_SECRET).compact()}`
+      const sub = faker.datatype.uuid();
+      const token = `Bearer ${njwt.create({ sub }, process.env.JWT_SECRET).compact()}`;
 
-      const title = faker.lorem.words()
-      const description = faker.lorem.text()
+      const title = faker.lorem.words();
+      const description = faker.lorem.text();
 
       const { status, body } = await request(server)
         .post('/tasks')
         .send({
           title,
-          description
+          description,
         })
-        .set('Authorization', token)
+        .set('Authorization', token);
 
-      expect(status).toBe(201)
+      expect(status).toBe(201);
       expect(body).toEqual({
         id: expect.any(Number),
         title,
@@ -63,120 +63,120 @@ describe('Tasks', () => {
         status: TaskStatus.TO_DO,
         ownerId: sub,
         createdAt: expect.any(String),
-        updatedAt: expect.any(String)
-      })
-    })
+        updatedAt: expect.any(String),
+      });
+    });
 
     it('Should not allow in case of invalid token', async () => {
-      const sub = faker.datatype.uuid()
+      const sub = faker.datatype.uuid();
       const token = faker.helpers.arrayElement([
         `Bearer ${njwt.create({ sub }, faker.datatype.string()).compact()}`,
-        ''
-      ])
+        '',
+      ]);
 
-      const title = faker.lorem.words()
-      const description = faker.lorem.text()
+      const title = faker.lorem.words();
+      const description = faker.lorem.text();
 
       const { status } = await request(server)
         .post('/tasks')
         .send({
           title,
-          description
+          description,
         })
-        .set('Authorization', token)
+        .set('Authorization', token);
 
-      expect(status).toBe(401)
-    })
-  })
+      expect(status).toBe(401);
+    });
+  });
 
   describe('GET /tasks', () => {
     it('Should list all owner\'s tasks', async () => {
-      const token1 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`
-      const token2 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`
+      const token1 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`;
+      const token2 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`;
 
       const { body: task } = await request(server)
         .post('/tasks')
         .set('Authorization', token1)
         .send({
           title: faker.lorem.words(),
-          description: faker.lorem.text()
-        })
+          description: faker.lorem.text(),
+        });
 
       await request(server)
         .post('/tasks')
         .set('Authorization', token2)
         .send({
           title: faker.lorem.words(),
-          description: faker.lorem.text()
-        })
+          description: faker.lorem.text(),
+        });
 
       const { status, body } = await request(server)
         .get('/tasks')
-        .set('Authorization', token1)
+        .set('Authorization', token1);
 
-      expect(status).toBe(200)
+      expect(status).toBe(200);
       expect(body).toEqual({
         data: [
-          task
+          task,
         ],
-        total: 1
-      })
-    })
-  })
+        total: 1,
+      });
+    });
+  });
 
   describe('PATCH /tasks/:taskId', () => {
     it('Should update the title, description or status of one task', async () => {
-      const token = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`
+      const token = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`;
 
       const { body: task } = await request(server)
         .post('/tasks')
         .set('Authorization', token)
         .send({
           title: faker.lorem.words(),
-          description: faker.lorem.text()
-        })
+          description: faker.lorem.text(),
+        });
 
-      const title = faker.lorem.words()
-      const description = faker.lorem.text()
+      const title = faker.lorem.words();
+      const description = faker.lorem.text();
 
       const { status, body } = await request(server)
         .patch(`/tasks/${task.id}`)
         .send({
           title,
           description,
-          status: faker.helpers.objectValue(TaskStatus)
+          status: faker.helpers.objectValue(TaskStatus),
         })
-        .set('Authorization', token)
+        .set('Authorization', token);
 
-      expect(status).toBe(200)
+      expect(status).toBe(200);
       expect(body).toEqual(expect.objectContaining({
         title,
-        description
-      }))
-    })
+        description,
+      }));
+    });
 
     it('Should not update other owner\'s task', async () => {
-      const token1 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`
-      const token2 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`
+      const token1 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`;
+      const token2 = `Bearer ${njwt.create({ sub: faker.datatype.uuid() }, process.env.JWT_SECRET).compact()}`;
 
       const { body: task } = await request(server)
         .post('/tasks')
         .set('Authorization', token1)
         .send({
           title: faker.lorem.words(),
-          description: faker.lorem.text()
-        })
+          description: faker.lorem.text(),
+        });
 
       const { status, body } = await request(server)
         .patch(`/tasks/${task.id}`)
         .send({
           title: faker.lorem.words(),
           description: faker.lorem.text(),
-          status: faker.helpers.objectValue(TaskStatus)
+          status: faker.helpers.objectValue(TaskStatus),
         })
-        .set('Authorization', token2)
+        .set('Authorization', token2);
 
-      expect(status).toBe(404)
-    })
-  })
-})
+      expect(status).toBe(404);
+    });
+  });
+});
